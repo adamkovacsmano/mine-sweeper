de361fbc1ce0cf0f0e0c101acf5637efa4cab27c
Adam Kovacs <adamkovacsmano@gmail.com>
2019-11-17 19:24:30 +0000

    field layout finished

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ console.log("hello");
 
 let gameField = document.getElementById("field");
 
-let array = new Array(4);
-console.log(array);
-
 const createField = (cols, rows, mine) => {
   let tombike2 = new Array(cols);
   for (let i = 0; i < tombike2.length; i++) {
@@ -33,16 +30,64 @@ const createField = (cols, rows, mine) => {
     }
   }
 
+  let clue = 0;
   for (let i = 0; i < cols; i++) {
     for (let k = 0; k < rows; k++) {
       if (tombike2[i][k].isLocked === false) {
-        let clue = 0;
         if (k < rows - 1 && tombike2[i][k + 1].isLocked === true) {
           clue++;
           tombike2[i][k].state = `${clue}`;
-          clue = 0;
+        }
+        if (
+          i - 1 >= 0 &&
+          i < cols - 1 &&
+          tombike2[i - 1][k].isLocked === true
+        ) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
+        }
+        if (
+          i - 1 >= 0 &&
+          k < rows - 1 &&
+          tombike2[i - 1][k + 1].isLocked === true
+        ) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
+        }
+        if (
+          i - 1 >= 0 &&
+          k - 1 >= 0 &&
+          tombike2[i - 1][k - 1].isLocked === true
+        ) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
+        }
+        if (k - 1 >= 0 && tombike2[i][k - 1].isLocked === true) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
+        }
+        if (
+          k - 1 >= 0 &&
+          i < cols - 1 &&
+          tombike2[i + 1][k - 1].isLocked === true
+        ) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
+        }
+        if (i < cols - 1 && tombike2[i + 1][k].isLocked === true) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
+        }
+        if (
+          i < cols - 1 &&
+          k < rows - 1 &&
+          tombike2[i + 1][k + 1].isLocked === true
+        ) {
+          clue++;
+          tombike2[i][k].state = `${clue}`;
         }
       }
+      clue = 0;
     }
   }
 
@@ -54,4 +99,12 @@ const createField = (cols, rows, mine) => {
   }
 };
 
-createField(5, 10, 12);
+createField(10, 10, 12);
+
+// for (let i = -1; i < 1; i++) {
+//   for (let j = -1 j < 1; j++) {
+//     // try catch?
+
+//   }
+
+// }
